c408d0284bc6978ea869de50648d461447040642
lunaru100 <lunaruteyvat@gmail.com>
2023-09-06 09:27:35 +0200

    added main activity class

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -1,4 +1,19 @@
 package com.example.firstapp;
 
-public class MainActivity {
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int hour = 9;
+        if (hour<12){
+            setContentView(R.layout.main_activity_layout_1);
+        }else{
+
+        }
+    }
 }
